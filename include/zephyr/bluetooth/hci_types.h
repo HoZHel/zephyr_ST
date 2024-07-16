zephyrproject-rtos/zephyr

include: Bluetooth: Add BT COD macros to hci_types.h
Add macros to get Major Service Classes, Major Device
Class and Minor Device Class.

Define Major Device Class code.
Define Minor Device Class field for Computer Major
Class.
Define Minor Device Class field for Phone Major Class.
Define Minor Device Class field for Audio/Video Major
Class.

Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

include/zephyr/bluetooth/hci_types.h
@@ -556,6 +556,65 @@ struct bt_hci_rp_write_conn_accept_timeout {
 #define BT_BREDR_SCAN_INQUIRY                   0x01
 #define BT_BREDR_SCAN_PAGE                      0x02
 
+#define BT_COD(major_service, major_device, mimor_device)                                          \
+	(((size_t)major_service << 13) | ((size_t)major_device << 8) || ((size_t)mimor_device << 2))
+#define BT_COD_VALID(cod) ((0 == (cod[0] & (BIT(0) | BIT(1)))) ? true : false)
+#define BT_COD_MAJOR_SERVICE_CLASSES(cod)                                                          \
+	((((size_t)cod[2] & 0xFF) >> 5) | (((size_t)cod[1] & 0xD0) >> 5))
+#define BT_COD_MAJOR_DEVICE_CLASS(cod) ((((size_t)cod[1]) & 0x1FUL))
+#define BT_COD_MINOR_DEVICE_CLASS(cod) (((((size_t)cod[0]) & 0xFF) >> 2))
+
+#define BT_COD_MAJOR_MISC           0x00
+#define BT_COD_MAJOR_COMPUTER       0x01
+#define BT_COD_MAJOR_PHONE          0x02
+#define BT_COD_MAJOR_LAN_NETWORK_AP 0x03
+#define BT_COD_MAJOR_AUDIO_VIDEO    0x04
+#define BT_COD_MAJOR_PERIPHERAL     0x05
+#define BT_COD_MAJOR_IMAGING        0x06
+#define BT_COD_MAJOR_WEARABLE       0x07
+#define BT_COD_MAJOR_TOY            0x08
+#define BT_COD_MAJOR_HEALTH         0x09
+#define BT_COD_MAJOR_UNCATEGORIZED  0x1F
+
+/* Minor Device Class field - Computer Major Class */
+#define BT_COD_MAJOR_COMPUTER_MINOR_UNCATEGORIZED         0x00
+#define BT_COD_MAJOR_COMPUTER_MINOR_DESKTOP               0x01
+#define BT_COD_MAJOR_COMPUTER_MINOR_SERVER_CLASS_COMPUTER 0x02
+#define BT_COD_MAJOR_COMPUTER_MINOR_LAPTOP                0x03
+#define BT_COD_MAJOR_COMPUTER_MINOR_HANDHELD_PC_PDA       0x04
+#define BT_COD_MAJOR_COMPUTER_MINOR_PALM_SIZE_PC_PDA      0x05
+#define BT_COD_MAJOR_COMPUTER_MINOR_WEARABLE_COMPUTER     0x06
+#define BT_COD_MAJOR_COMPUTER_MINOR_TABLET                0x07
+
+/* Minor Device Class field - Phone Major Class */
+#define BT_COD_MAJOR_PHONE_MINOR_UNCATEGORIZED             0x00
+#define BT_COD_MAJOR_PHONE_MINOR_CELLULAR                  0x01
+#define BT_COD_MAJOR_PHONE_MINOR_CORDLESS                  0x02
+#define BT_COD_MAJOR_PHONE_MINOR_STARTPHONE                0x03
+#define BT_COD_MAJOR_PHONE_MINOR_WIRED_MODEM_VOICE_GATEWAY 0x04
+#define BT_COD_MAJOR_PHONE_MINOR_ISDN                      0x05
+
+/* Minor Device Class field - Audio/Video Major Class */
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_UNCATEGORIZED             0x00
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_WEARABLE_HEADSET          0x01
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_HANDS_FREE                0x02
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_RFU                       0x03
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_MICROPHONE                0x04
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_LOUDSPEAKER               0x05
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_HEADPHONES                0x06
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_PORTABLE_AUDIO            0x07
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_CAR_AUDIO                 0x08
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_SET_TOP_BOX               0x09
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_HIFI_AUDIO                0x0A
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_VCR                       0x0B
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_VIDEO_CAMERA              0x0C
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_CAMCORDER                 0x0D
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_VIDEO_MONITOR             0x0E
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_VIDEO_DISPLAY_LOUDSPEAKER 0x0F
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_VIDEO_CONFERENCING        0x10
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_RFU2                      0x11
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_GAME_TOY                  0x12
+
 #define BT_HCI_OP_WRITE_CLASS_OF_DEVICE         BT_OP(BT_OGF_BASEBAND, 0x0024) /* 0x0c24 */
 struct bt_hci_cp_write_class_of_device {
 	uint8_t  class_of_device[3];

HoZHel/zephyr_ST

include/zephyr/bluetooth/hci_types.h
@@ -567,6 +567,66 @@ struct bt_hci_rp_write_conn_accept_timeout {
 #define BT_BREDR_SCAN_INQUIRY                   0x01
 #define BT_BREDR_SCAN_PAGE                      0x02
 
+#define BT_COD(major_service, major_device, minor_device)                         \
+	(((uint32_t)major_service << 13) | ((uint32_t)major_device << 8) |            \
+	 ((uint32_t)minor_device << 2))
+#define BT_COD_VALID(cod) ((0 == (cod[0] & (BIT(0) | BIT(1)))) ? true : false)
+#define BT_COD_MAJOR_SERVICE_CLASSES(cod)                                         \
+	((((uint32_t)cod[2] & 0xFF) >> 5) | (((uint32_t)cod[1] & 0xD0) >> 5))
+#define BT_COD_MAJOR_DEVICE_CLASS(cod) ((((uint32_t)cod[1]) & 0x1FUL))
+#define BT_COD_MINOR_DEVICE_CLASS(cod) (((((uint32_t)cod[0]) & 0xFF) >> 2))
+
+#define BT_COD_MAJOR_MISC           0x00
+#define BT_COD_MAJOR_COMPUTER       0x01
+#define BT_COD_MAJOR_PHONE          0x02
+#define BT_COD_MAJOR_LAN_NETWORK_AP 0x03
+#define BT_COD_MAJOR_AUDIO_VIDEO    0x04
+#define BT_COD_MAJOR_PERIPHERAL     0x05
+#define BT_COD_MAJOR_IMAGING        0x06
+#define BT_COD_MAJOR_WEARABLE       0x07
+#define BT_COD_MAJOR_TOY            0x08
+#define BT_COD_MAJOR_HEALTH         0x09
+#define BT_COD_MAJOR_UNCATEGORIZED  0x1F
+
+/* Minor Device Class field - Computer Major Class */
+#define BT_COD_MAJOR_COMPUTER_MINOR_UNCATEGORIZED         0x00
+#define BT_COD_MAJOR_COMPUTER_MINOR_DESKTOP               0x01
+#define BT_COD_MAJOR_COMPUTER_MINOR_SERVER_CLASS_COMPUTER 0x02
+#define BT_COD_MAJOR_COMPUTER_MINOR_LAPTOP                0x03
+#define BT_COD_MAJOR_COMPUTER_MINOR_HANDHELD_PC_PDA       0x04
+#define BT_COD_MAJOR_COMPUTER_MINOR_PALM_SIZE_PC_PDA      0x05
+#define BT_COD_MAJOR_COMPUTER_MINOR_WEARABLE_COMPUTER     0x06
+#define BT_COD_MAJOR_COMPUTER_MINOR_TABLET                0x07
+
+/* Minor Device Class field - Phone Major Class */
+#define BT_COD_MAJOR_PHONE_MINOR_UNCATEGORIZED             0x00
+#define BT_COD_MAJOR_PHONE_MINOR_CELLULAR                  0x01
+#define BT_COD_MAJOR_PHONE_MINOR_CORDLESS                  0x02
+#define BT_COD_MAJOR_PHONE_MINOR_SMARTPHONE                0x03
+#define BT_COD_MAJOR_PHONE_MINOR_WIRED_MODEM_VOICE_GATEWAY 0x04
+#define BT_COD_MAJOR_PHONE_MINOR_ISDN                      0x05
+
+/* Minor Device Class field - Audio/Video Major Class */
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_UNCATEGORIZED             0x00
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_WEARABLE_HEADSET          0x01
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_HANDS_FREE                0x02
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_RFU                       0x03
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_MICROPHONE                0x04
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_LOUDSPEAKER               0x05
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_HEADPHONES                0x06
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_PORTABLE_AUDIO            0x07
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_CAR_AUDIO                 0x08
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_SET_TOP_BOX               0x09
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_HIFI_AUDIO                0x0A
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_VCR                       0x0B
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_VIDEO_CAMERA              0x0C
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_CAMCORDER                 0x0D
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_VIDEO_MONITOR             0x0E
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_VIDEO_DISPLAY_LOUDSPEAKER 0x0F
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_VIDEO_CONFERENCING        0x10
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_RFU2                      0x11
+#define BT_COD_MAJOR_AUDIO_VIDEO_MINOR_GAME_TOY                  0x12
+
 #define BT_HCI_OP_WRITE_CLASS_OF_DEVICE         BT_OP(BT_OGF_BASEBAND, 0x0024) /* 0x0c24 */
 struct bt_hci_cp_write_class_of_device {
 	uint8_t  class_of_device[3];