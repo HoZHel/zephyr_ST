dztang/zephyr

drivers: led_strip: Add length function
Adds a length function which returns the length of the LED strip

Signed-off-by: Jamie McCrae <spam@helper3000.net>

drivers/led_strip/apa102.c
@@ -14,6 +14,7 @@
 
 struct apa102_config {
 	struct spi_dt_spec bus;
+	uint16_t length;
 };
 
 static int apa102_update(const struct device *dev, void *buf, size_t size)
@@ -80,6 +81,17 @@ static int apa102_update_channels(const struct device *dev, uint8_t *channels,
 	return -EINVAL;
 }
 
+static int apa102_length(const struct device *dev)
+{
+	const struct apa102_config *config = dev->config;
+
+	if (!spi_is_ready_dt(&config->bus)) {
+		return -ENODEV;
+	}
+
+	return config->length;
+}
+
 static int apa102_init(const struct device *dev)
 {
 	const struct apa102_config *config = dev->config;
@@ -94,6 +106,7 @@ static int apa102_init(const struct device *dev)
 static const struct led_strip_driver_api apa102_api = {
 	.update_rgb = apa102_update_rgb,
 	.update_channels = apa102_update_channels,
+	.length = apa102_length,
 };
 
 #define APA102_DEVICE(idx)						 \
@@ -102,6 +115,7 @@ static const struct led_strip_driver_api apa102_api = {
 			idx,						 \
 			SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8), \
 			0),						 \
+		.length = DT_INST_PROP(idx, chain_length),		 \
 	};								 \
 									 \
 	DEVICE_DT_INST_DEFINE(idx,					 \

drivers/led_strip/lpd880x.c
@@ -36,6 +36,7 @@ LOG_MODULE_REGISTER(lpd880x);
 
 struct lpd880x_config {
 	struct spi_dt_spec bus;
+	uint16_t length;
 };
 
 static int lpd880x_update(const struct device *dev, void *data, size_t size)
@@ -125,6 +126,17 @@ static int lpd880x_strip_update_channels(const struct device *dev,
 	return lpd880x_update(dev, channels, num_channels);
 }
 
+static int lpd880x_strip_length(const struct device *dev)
+{
+	const struct lpd880x_config *config = dev->config;
+
+	if (!spi_is_ready_dt(&config->bus)) {
+		return -ENODEV;
+	}
+
+	return config->length;
+}
+
 static int lpd880x_strip_init(const struct device *dev)
 {
 	const struct lpd880x_config *config = dev->config;
@@ -138,12 +150,14 @@ static int lpd880x_strip_init(const struct device *dev)
 }
 
 static const struct lpd880x_config lpd880x_config = {
-	.bus = SPI_DT_SPEC_INST_GET(0, LPD880X_SPI_OPERATION, 0)
+	.bus = SPI_DT_SPEC_INST_GET(0, LPD880X_SPI_OPERATION, 0),
+	.length = DT_INST_PROP(0, chain_length),
 };
 
 static const struct led_strip_driver_api lpd880x_strip_api = {
 	.update_rgb = lpd880x_strip_update_rgb,
 	.update_channels = lpd880x_strip_update_channels,
+	.length = lpd880x_strip_length,
 };
 
 DEVICE_DT_INST_DEFINE(0, lpd880x_strip_init, NULL,

drivers/led_strip/ws2812_rpi_pico_pio.c
@@ -24,6 +24,7 @@ struct ws2812_led_strip_config {
 	const struct device *piodev;
 	const uint8_t gpio_pin;
 	uint8_t num_colors;
+	uint16_t length;
 	uint32_t frequency;
 	const uint8_t *const color_mapping;
 	uint16_t reset_delay;
@@ -115,9 +116,21 @@ static int ws2812_led_strip_update_channels(const struct device *dev, uint8_t *c
 	return -ENOTSUP;
 }
 
+static int ws2812_led_strip_length(const struct device *dev)
+{
+	const struct ws2812_led_strip_config *config = dev->config;
+
+	if (!device_is_ready(config->piodev)) {
+		return -ENODEV;
+	}
+
+	return config->length;
+}
+
 static const struct led_strip_driver_api ws2812_led_strip_api = {
 	.update_rgb = ws2812_led_strip_update_rgb,
 	.update_channels = ws2812_led_strip_update_channels,
+	.length = ws2812_led_strip_length,
 };
 
 /*
@@ -190,6 +203,7 @@ static int ws2812_rpi_pico_pio_init(const struct device *dev)
 		.piodev = DEVICE_DT_GET(DT_PARENT(DT_PARENT(node))),                               \
 		.gpio_pin = DT_GPIO_PIN_BY_IDX(node, gpios, 0),                                    \
 		.num_colors = DT_PROP_LEN(node, color_mapping),                                    \
+		.length = DT_INST_PROP(node, chain_length),                                        \
 		.color_mapping = ws2812_led_strip_##node##_color_mapping,                          \
 		.reset_delay = DT_PROP(node, reset_delay),                                         \
 		.frequency = DT_PROP(node, frequency),                                             \

drivers/led_strip/ws2812_spi.c
@@ -44,6 +44,7 @@ struct ws2812_spi_cfg {
 	uint8_t zero_frame;
 	uint8_t num_colors;
 	const uint8_t *color_mapping;
+	uint16_t length;
 	uint16_t reset_delay;
 };
 
@@ -160,6 +161,17 @@ static int ws2812_strip_update_channels(const struct device *dev,
 	return -ENOTSUP;
 }
 
+static int ws2812_strip_length(const struct device *dev)
+{
+	const struct ws2812_spi_cfg *cfg = dev_cfg(dev);
+
+	if (!spi_is_ready_dt(&cfg->bus)) {
+		return -ENODEV;
+	}
+
+	return config->length;
+}
+
 static int ws2812_spi_init(const struct device *dev)
 {
 	const struct ws2812_spi_cfg *cfg = dev_cfg(dev);
@@ -191,6 +203,7 @@ static int ws2812_spi_init(const struct device *dev)
 static const struct led_strip_driver_api ws2812_spi_api = {
 	.update_rgb = ws2812_strip_update_rgb,
 	.update_channels = ws2812_strip_update_channels,
+	.length = ws2812_strip_length,
 };
 
 #define WS2812_SPI_NUM_PIXELS(idx) \
@@ -231,6 +244,7 @@ static const struct led_strip_driver_api ws2812_spi_api = {
 		.zero_frame = WS2812_SPI_ZERO_FRAME(idx),		 \
 		.num_colors = WS2812_NUM_COLORS(idx),			 \
 		.color_mapping = ws2812_spi_##idx##_color_mapping,	 \
+		.length = DT_INST_PROP(idx, chain_length),               \
 		.reset_delay = WS2812_RESET_DELAY(idx),			 \
 	};								 \
 									 \

HoZHel/zephyr_ST

drivers/led_strip/apa102.c
@@ -14,6 +14,7 @@
 
 struct apa102_config {
 	struct spi_dt_spec bus;
+	size_t length;
 };
 
 static int apa102_update(const struct device *dev, void *buf, size_t size)
@@ -80,6 +81,13 @@ static int apa102_update_channels(const struct device *dev, uint8_t *channels,
 	return -EINVAL;
 }
 
+static size_t apa102_length(const struct device *dev)
+{
+	const struct apa102_config *config = dev->config;
+
+	return config->length;
+}
+
 static int apa102_init(const struct device *dev)
 {
 	const struct apa102_config *config = dev->config;
@@ -94,6 +102,7 @@ static int apa102_init(const struct device *dev)
 static const struct led_strip_driver_api apa102_api = {
 	.update_rgb = apa102_update_rgb,
 	.update_channels = apa102_update_channels,
+	.length = apa102_length,
 };
 
 #define APA102_DEVICE(idx)						 \
@@ -102,6 +111,7 @@ static const struct led_strip_driver_api apa102_api = {
 			idx,						 \
 			SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8), \
 			0),						 \
+		.length = DT_INST_PROP(idx, chain_length),		 \
 	};								 \
 									 \
 	DEVICE_DT_INST_DEFINE(idx,					 \

drivers/led_strip/lpd880x.c
@@ -36,6 +36,7 @@ LOG_MODULE_REGISTER(lpd880x);
 
 struct lpd880x_config {
 	struct spi_dt_spec bus;
+	size_t length;
 };
 
 static int lpd880x_update(const struct device *dev, void *data, size_t size)
@@ -125,6 +126,13 @@ static int lpd880x_strip_update_channels(const struct device *dev,
 	return lpd880x_update(dev, channels, num_channels);
 }
 
+static size_t lpd880x_strip_length(const struct device *dev)
+{
+	const struct lpd880x_config *config = dev->config;
+
+	return config->length;
+}
+
 static int lpd880x_strip_init(const struct device *dev)
 {
 	const struct lpd880x_config *config = dev->config;
@@ -138,12 +146,14 @@ static int lpd880x_strip_init(const struct device *dev)
 }
 
 static const struct lpd880x_config lpd880x_config = {
-	.bus = SPI_DT_SPEC_INST_GET(0, LPD880X_SPI_OPERATION, 0)
+	.bus = SPI_DT_SPEC_INST_GET(0, LPD880X_SPI_OPERATION, 0),
+	.length = DT_INST_PROP(0, chain_length),
 };
 
 static const struct led_strip_driver_api lpd880x_strip_api = {
 	.update_rgb = lpd880x_strip_update_rgb,
 	.update_channels = lpd880x_strip_update_channels,
+	.length = lpd880x_strip_length,
 };
 
 DEVICE_DT_INST_DEFINE(0, lpd880x_strip_init, NULL,

drivers/led_strip/ws2812_rpi_pico_pio.c
@@ -24,6 +24,7 @@ struct ws2812_led_strip_config {
 	const struct device *piodev;
 	const uint8_t gpio_pin;
 	uint8_t num_colors;
+	size_t length;
 	uint32_t frequency;
 	const uint8_t *const color_mapping;
 	uint16_t reset_delay;
@@ -115,9 +116,17 @@ static int ws2812_led_strip_update_channels(const struct device *dev, uint8_t *c
 	return -ENOTSUP;
 }
 
+static size_t ws2812_led_strip_length(const struct device *dev)
+{
+	const struct ws2812_led_strip_config *config = dev->config;
+
+	return config->length;
+}
+
 static const struct led_strip_driver_api ws2812_led_strip_api = {
 	.update_rgb = ws2812_led_strip_update_rgb,
 	.update_channels = ws2812_led_strip_update_channels,
+	.length = ws2812_led_strip_length,
 };
 
 /*
@@ -190,6 +199,7 @@ static int ws2812_rpi_pico_pio_init(const struct device *dev)
 		.piodev = DEVICE_DT_GET(DT_PARENT(DT_PARENT(node))),                               \
 		.gpio_pin = DT_GPIO_PIN_BY_IDX(node, gpios, 0),                                    \
 		.num_colors = DT_PROP_LEN(node, color_mapping),                                    \
+		.length = DT_PROP(node, chain_length),                                             \
 		.color_mapping = ws2812_led_strip_##node##_color_mapping,                          \
 		.reset_delay = DT_PROP(node, reset_delay),                                         \
 		.frequency = DT_PROP(node, frequency),                                             \

drivers/led_strip/ws2812_spi.c
@@ -44,6 +44,7 @@ struct ws2812_spi_cfg {
 	uint8_t zero_frame;
 	uint8_t num_colors;
 	const uint8_t *color_mapping;
+	size_t length;
 	uint16_t reset_delay;
 };
 
@@ -160,6 +161,13 @@ static int ws2812_strip_update_channels(const struct device *dev,
 	return -ENOTSUP;
 }
 
+static size_t ws2812_strip_length(const struct device *dev)
+{
+	const struct ws2812_spi_cfg *cfg = dev_cfg(dev);
+
+	return cfg->length;
+}
+
 static int ws2812_spi_init(const struct device *dev)
 {
 	const struct ws2812_spi_cfg *cfg = dev_cfg(dev);
@@ -191,6 +199,7 @@ static int ws2812_spi_init(const struct device *dev)
 static const struct led_strip_driver_api ws2812_spi_api = {
 	.update_rgb = ws2812_strip_update_rgb,
 	.update_channels = ws2812_strip_update_channels,
+	.length = ws2812_strip_length,
 };
 
 #define WS2812_SPI_NUM_PIXELS(idx) \
@@ -231,6 +240,7 @@ static const struct led_strip_driver_api ws2812_spi_api = {
 		.zero_frame = WS2812_SPI_ZERO_FRAME(idx),		 \
 		.num_colors = WS2812_NUM_COLORS(idx),			 \
 		.color_mapping = ws2812_spi_##idx##_color_mapping,	 \
+		.length = DT_INST_PROP(idx, chain_length),               \
 		.reset_delay = WS2812_RESET_DELAY(idx),			 \
 	};								 \
 									 \