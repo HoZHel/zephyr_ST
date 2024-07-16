dztang/zephyr

drivers: clock_control: Update NXP LPC Syscon driver to add FlexIO
Add support to get FlexIO clock

Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h
@@ -81,4 +81,6 @@
 #define MCUX_LPADC1_CLK			MCUX_LPC_CLK_ID(0x0F, 0x00)
 #define MCUX_LPADC2_CLK			MCUX_LPC_CLK_ID(0x0F, 0x01)
 
+#define MCUX_FLEXIO0_CLK		MCUX_LPC_CLK_ID(0x10, 0x00)
+
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_MCUX_LPC_SYSCON_H_ */

HoZHel/zephyr_ST

include/zephyr/dt-bindings/clock/mcux_lpc_syscon_clock.h
@@ -87,4 +87,6 @@
 #define MCUX_FLEXCAN0_CLK		MCUX_LPC_CLK_ID(0x10, 0x00)
 #define MCUX_FLEXCAN1_CLK		MCUX_LPC_CLK_ID(0x10, 0x01)
 
+#define MCUX_FLEXIO0_CLK		MCUX_LPC_CLK_ID(0x11, 0x00)
+
 #endif /* ZEPHYR_INCLUDE_DT_BINDINGS_CLOCK_MCUX_LPC_SYSCON_H_ */