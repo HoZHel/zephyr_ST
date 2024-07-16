cfriedt/zephyr

posix: features: leave feature test macros undefined if unimpl
Rather than forcing the macro to -1, simply leave it undefined
if the particular feature in question is unimplemented. This is
the convention used by external libcs.

Signed-off-by: Chris Friedt <cfriedt@tenstorrent.com>

include/zephyr/posix/posix_features.h
@@ -11,101 +11,181 @@
 #include <autoconf.h>              /* CONFIG_* */
 #include <zephyr/sys/util_macro.h> /* COND_CODE_1() */
 
-/* Version test macros */
-#define _POSIX_VERSION  200809L
-#define _POSIX2_VERSION (-1L)
-#define _XOPEN_VERSION  (-1L)
+/*
+ * POSIX System Interfaces
+ */
 
-/* Internal helper macro to set constant if required Kconfig symbol is enabled */
-#define Z_SC_VAL_IFDEF(_def, _val) COND_CODE_1(_def, (_val), (-1L))
+#define _POSIX_VERSION 200809L
 
-/* Constants for Options and Option Groups */
-#define _POSIX_ADVISORY_INFO              (-1L)
-#define _POSIX_ASYNCHRONOUS_IO \
-	Z_SC_VAL_IFDEF(CONFIG_POSIX_ASYNCHRONOUS_IO, _POSIX_VERSION)
-#define _POSIX_BARRIERS                   Z_SC_VAL_IFDEF(CONFIG_PTHREAD_BARRIER, _POSIX_VERSION)
-#define _POSIX_CHOWN_RESTRICTED           (0)
-#define _POSIX_CLOCK_SELECTION            Z_SC_VAL_IFDEF(CONFIG_POSIX_CLOCK, _POSIX_VERSION)
-#define _POSIX_CPUTIME                    (-1L)
-#define _POSIX_FSYNC                      Z_SC_VAL_IFDEF(CONFIG_POSIX_FSYNC, _POSIX_VERSION)
-#define _POSIX_IPV6                       Z_SC_VAL_IFDEF(CONFIG_NET_IPV6, _POSIX_VERSION)
-#define _POSIX_JOB_CONTROL                (-1L)
-#define _POSIX_MAPPED_FILES               (-1L)
-#define _POSIX_MEMLOCK                    (-1L)
-#define _POSIX_MEMLOCK_RANGE              (-1L)
-#define _POSIX_MEMORY_PROTECTION          (-1L)
-#define _POSIX_MESSAGE_PASSING            Z_SC_VAL_IFDEF(CONFIG_POSIX_MQUEUE, _POSIX_VERSION)
-#define _POSIX_MONOTONIC_CLOCK            Z_SC_VAL_IFDEF(CONFIG_POSIX_CLOCK, _POSIX_VERSION)
-#define _POSIX_NO_TRUNC                   (0)
-#define _POSIX_PRIORITIZED_IO             (-1L)
-#define _POSIX_PRIORITY_SCHEDULING \
-	Z_SC_VAL_IFDEF(CONFIG_POSIX_PRIORITY_SCHEDULING, _POSIX_VERSION)
-#define _POSIX_RAW_SOCKETS                Z_SC_VAL_IFDEF(CONFIG_NET_SOCKETS_PACKET, _POSIX_VERSION)
-#define _POSIX_READER_WRITER_LOCKS        Z_SC_VAL_IFDEF(CONFIG_PTHREAD_IPC, _POSIX_VERSION)
-#define _POSIX_REALTIME_SIGNALS           (-1L)
-#define _POSIX_REGEXP                     (-1L)
-#define _POSIX_SAVED_IDS                  (-1L)
-#define _POSIX_SEMAPHORES                 Z_SC_VAL_IFDEF(CONFIG_PTHREAD_IPC, _POSIX_VERSION)
-#define _POSIX_SHARED_MEMORY_OBJECTS      (-1L)
-#define _POSIX_SHELL                      (-1L)
-#define _POSIX_SPAWN                      (-1L)
-#define _POSIX_SPIN_LOCKS                 Z_SC_VAL_IFDEF(CONFIG_PTHREAD_SPINLOCK, _POSIX_VERSION)
-#define _POSIX_SPORADIC_SERVER            (-1L)
-#define _POSIX_SYNCHRONIZED_IO            (-1L)
-#define _POSIX_THREAD_ATTR_STACKADDR      Z_SC_VAL_IFDEF(CONFIG_PTHREAD_IPC, _POSIX_VERSION)
-#define _POSIX_THREAD_ATTR_STACKSIZE      Z_SC_VAL_IFDEF(CONFIG_PTHREAD_IPC, _POSIX_VERSION)
-#define _POSIX_THREAD_CPUTIME             (-1L)
-#define _POSIX_THREAD_PRIO_INHERIT        _POSIX_VERSION
-#define _POSIX_THREAD_PRIO_PROTECT        (-1L)
-#define _POSIX_THREAD_PRIORITY_SCHEDULING Z_SC_VAL_IFDEF(CONFIG_PTHREAD_IPC, _POSIX_VERSION)
-#define _POSIX_THREAD_PROCESS_SHARED      (-1L)
-#define _POSIX_THREAD_ROBUST_PRIO_INHERIT (-1L)
-#define _POSIX_THREAD_ROBUST_PRIO_PROTECT (-1L)
-#define _POSIX_THREAD_SAFE_FUNCTIONS      Z_SC_VAL_IFDEF(CONFIG_PTHREAD_IPC, _POSIX_VERSION)
-#define _POSIX_THREAD_SPORADIC_SERVER     (-1L)
+#define _POSIX_CHOWN_RESTRICTED (0)
+#define _POSIX_NO_TRUNC         (0)
+#define _POSIX_VDISABLE         ('\0')
 
-#ifndef _POSIX_THREADS
-#define _POSIX_THREADS Z_SC_VAL_IFDEF(CONFIG_PTHREAD_IPC, _POSIX_VERSION)
+/* #define _POSIX_ADVISORY_INFO (-1L) */
+
+#ifdef CONFIG_POSIX_ASYNCHRONOUS_IO
+#define _POSIX_ASYNCHRONOUS_IO _POSIX_VERSION
 #endif
 
-#define _POSIX_TIMEOUTS             Z_SC_VAL_IFDEF(CONFIG_POSIX_CLOCK, _POSIX_VERSION)
-#define _POSIX_TIMERS               Z_SC_VAL_IFDEF(CONFIG_POSIX_CLOCK, _POSIX_VERSION)
-#define _POSIX_TRACE                (-1L)
-#define _POSIX_TRACE_EVENT_FILTER   (-1L)
-#define _POSIX_TRACE_INHERIT        (-1L)
-#define _POSIX_TRACE_LOG            (-1L)
-#define _POSIX_TYPED_MEMORY_OBJECTS (-1L)
-#define _POSIX_V6_ILP32_OFF32       (-1L)
-#define _POSIX_V6_ILP32_OFFBIG      (-1L)
-#define _POSIX_V6_LP64_OFF64        (-1L)
-#define _POSIX_V6_LPBIG_OFFBIG      (-1L)
-#define _POSIX_V7_ILP32_OFF32       (-1L)
-#define _POSIX_V7_ILP32_OFFBIG      (-1L)
-#define _POSIX_V7_LP64_OFF64        (-1L)
-#define _POSIX_V7_LPBIG_OFFBIG      (-1L)
-#define _POSIX2_C_BIND              _POSIX_VERSION
-#define _POSIX2_C_DEV               (-1L)
-#define _POSIX2_CHAR_TERM           (-1L)
-#define _POSIX2_DELAYTIMER_MAX      (-1L)
-#define _POSIX2_FORT_DEV            (-1L)
-#define _POSIX2_FORT_RUN            (-1L)
-#define _POSIX2_LOCALEDEF           (-1L)
-#define _POSIX2_PBS                 (-1L)
-#define _POSIX2_PBS_ACCOUNTING      (-1L)
-#define _POSIX2_PBS_CHECKPOINT      (-1L)
-#define _POSIX2_PBS_LOCATE          (-1L)
-#define _POSIX2_PBS_MESSAGE         (-1L)
-#define _POSIX2_PBS_TRACK           (-1L)
-#define _POSIX2_SW_DEV              (-1L)
-#define _POSIX2_UPE                 (-1L)
-#define _XOPEN_CRYPT                (-1L)
-#define _XOPEN_ENH_I18N             (-1L)
-#define _XOPEN_REALTIME             (-1L)
-#define _XOPEN_REALTIME_THREADS     (-1L)
-#define _XOPEN_SHM                  (-1L)
-#define _XOPEN_STREAMS              (-1L)
-#define _XOPEN_UNIX                 (-1L)
-#define _XOPEN_UUCP                 (-1L)
+#ifdef CONFIG_PTHREAD_BARRIER
+#define _POSIX_BARRIERS _POSIX_VERSION
+#endif
+
+#ifdef CONFIG_POSIX_CLOCK
+#define _POSIX_CLOCK_SELECTION _POSIX_VERSION
+#endif
+
+/* #define _POSIX_CPUTIME (-1L) */
+
+#ifdef CONFIG_POSIX_FSYNC
+#define _POSIX_FSYNC _POSIX_VERSION
+#endif
+
+#ifdef CONFIG_NET_IPV6
+#define _POSIX_IPV6 _POSIX_VERSION
+#endif
+
+/* #define _POSIX_JOB_CONTROL (-1L) */
+/* #define _POSIX_MAPPED_FILES (-1L) */
+/* #define _POSIX_MEMLOCK (-1L) */
+/* #define _POSIX_MEMLOCK_RANGE (-1L) */
+/* #define _POSIX_MEMORY_PROTECTION (-1L) */
+
+#ifdef CONFIG_POSIX_MQUEUE
+#define _POSIX_MESSAGE_PASSING _POSIX_VERSION
+#endif
+
+#ifdef CONFIG_POSIX_CLOCK
+#define _POSIX_MONOTONIC_CLOCK _POSIX_VERSION
+#endif
+
+/* #define _POSIX_PRIORITIZED_IO (-1L) */
+
+#ifdef CONFIG_POSIX_PRIORITY_SCHEDULING
+#define _POSIX_PRIORITY_SCHEDULING _POSIX_VERSION
+#endif
+
+#ifdef CONFIG_NET_SOCKETS_PACKET
+#define _POSIX_RAW_SOCKETS _POSIX_VERSION
+#endif
+
+#ifdef CONFIG_PTHREAD_IPC
+#define _POSIX_READER_WRITER_LOCKS _POSIX_VERSION
+#endif
+
+/* #define _POSIX_REALTIME_SIGNALS (-1L) */
+/* #define _POSIX_REGEXP (-1L) */
+/* #define _POSIX_SAVED_IDS (-1L) */
+
+#ifdef CONFIG_PTHREAD_IPC
+#define _POSIX_SEMAPHORES _POSIX_VERSION
+#endif
+
+/* #define _POSIX_SHARED_MEMORY_OBJECTS (-1L) */
+/* #define _POSIX_SHELL (-1L) */
+/* #define _POSIX_SPAWN (-1L) */
+
+#ifdef CONFIG_PTHREAD_SPINLOCK
+#define _POSIX_SPIN_LOCKS _POSIX_VERSION
+#endif
+
+/* #define _POSIX_SPORADIC_SERVER (-1L) */
+/* #define _POSIX_SYNCHRONIZED_IO (-1L) */
+
+#ifdef CONFIG_PTHREAD_IPC
+#define _POSIX_THREAD_ATTR_STACKADDR _POSIX_VERSION
+#endif
+
+#ifdef CONFIG_PTHREAD_IPC
+#define _POSIX_THREAD_ATTR_STACKSIZE _POSIX_VERSION
+#endif
+
+/* #define _POSIX_THREAD_CPUTIME		  (-1L) */
+
+#ifdef CONFIG_PTHREAD_IPC
+#define _POSIX_THREAD_PRIO_INHERIT _POSIX_VERSION
+#endif
+
+/* #define _POSIX_THREAD_PRIO_PROTECT (-1L) */
+/* #define _POSIX_THREAD_PRIORITY_SCHEDULING (-1L) */
+/* #define _POSIX_THREAD_PROCESS_SHARED (-1L) */
+/* #define _POSIX_THREAD_ROBUST_PRIO_INHERIT (-1L) */
+/* #define _POSIX_THREAD_ROBUST_PRIO_PROTECT (-1L) */
+
+#ifdef CONFIG_PTHREAD_IPC
+#define _POSIX_THREAD_SAFE_FUNCTIONS _POSIX_VERSION
+#endif
+
+/* #define _POSIX_THREAD_SPORADIC_SERVER (-1L) */
+
+#ifdef CONFIG_PTHREAD_IPC
+#ifndef _POSIX_THREADS
+#define _POSIX_THREADS _POSIX_VERSION
+#endif
+#endif
+
+#ifdef CONFIG_POSIX_CLOCK
+#define _POSIX_TIMEOUTS _POSIX_VERSION
+#endif
+
+#ifdef CONFIG_POSIX_CLOCK
+#define _POSIX_TIMERS _POSIX_VERSION
+#endif
+
+/* #define _POSIX_TRACE (-1L) */
+/* #define _POSIX_TRACE_EVENT_FILTER (-1L) */
+/* #define _POSIX_TRACE_INHERIT (-1L) */
+/* #define _POSIX_TRACE_LOG (-1L) */
+/* #define _POSIX_TYPED_MEMORY_OBJECTS (-1L) */
+
+/*
+ * POSIX v6 Options
+ */
+/* #define _POSIX_V6_ILP32_OFF32 (-1L) */
+/* #define _POSIX_V6_ILP32_OFFBIG (-1L) */
+/* #define _POSIX_V6_LP64_OFF64 (-1L) */
+/* #define _POSIX_V6_LPBIG_OFFBIG (-1L) */
+
+/*
+ * POSIX v7 Options
+ */
+/* #define _POSIX_V7_ILP32_OFF32 (-1L) */
+/* #define _POSIX_V7_ILP32_OFFBIG (-1L) */
+/* #define _POSIX_V7_LP64_OFF64 (-1L) */
+/* #define _POSIX_V7_LPBIG_OFFBIG (-1L) */
+
+/*
+ * POSIX2 Options
+ */
+#define _POSIX2_VERSION _POSIX_VERSION
+#define _POSIX2_C_BIND  _POSIX2_VERSION
+#define _POSIX2_C_DEV   _POSIX2_VERSION
+/* #define _POSIX2_CHAR_TERM (-1L) */
+/* #define _POSIX2_DELAYTIMER_MAX (-1L) */
+/* #define _POSIX2_FORT_DEV (-1L) */
+/* #define _POSIX2_FORT_RUN (-1L) */
+/* #define _POSIX2_LOCALEDEF (-1L) */
+/* #define _POSIX2_PBS (-1L) */
+/* #define _POSIX2_PBS_ACCOUNTING (-1L) */
+/* #define _POSIX2_PBS_CHECKPOINT (-1L) */
+/* #define _POSIX2_PBS_LOCATE (-1L) */
+/* #define _POSIX2_PBS_MESSAGE (-1L) */
+/* #define _POSIX2_PBS_TRACK (-1L) */
+/* #define _POSIX2_SW_DEV (-1L) */
+/* #define _POSIX2_UPE (-1L) */
+
+/*
+ * X/Open System Interfaces
+ */
+#define _XOPEN_VERSION 700
+/* #define _XOPEN_CRYPT (-1L) */
+/* #define _XOPEN_ENH_I18N (-1L) */
+/* #define _XOPEN_REALTIME (-1L) */
+/* #define _XOPEN_REALTIME_THREADS (-1L) */
+/* #define _XOPEN_SHM (-1L) */
+/* #define _XOPEN_STREAMS (-1L) */
+/* #define _XOPEN_UNIX (-1L) */
+/* #define _XOPEN_UUCP (-1L) */
 
 /* Maximum values */
 #define _POSIX_CLOCKRES_MIN (20000000L)
@@ -115,14 +195,16 @@
 #define _POSIX_AIO_MAX                      (1)
 #define _POSIX_ARG_MAX                      (4096)
 #define _POSIX_CHILD_MAX                    (25)
-#define _POSIX_DELAYTIMER_MAX               (32)
-#define _POSIX_HOST_NAME_MAX                (255)
+#define _POSIX_DELAYTIMER_MAX \
+	COND_CODE_1(CONFIG_POSIX_TIMERS, (CONFIG_POSIX_DELAYTIMER_MAX), (0))
+#define _POSIX_HOST_NAME_MAX \
+	COND_CODE_1(CONFIG_NETWORKING, (NET_HOSTNAME_MAX_LEN), (0))
 #define _POSIX_LINK_MAX                     (8)
 #define _POSIX_LOGIN_NAME_MAX               (9)
 #define _POSIX_MAX_CANON                    (255)
 #define _POSIX_MAX_INPUT                    (255)
 #define _POSIX_MQ_OPEN_MAX \
-	Z_SC_VAL_IFDEF(CONFIG_POSIX_MQUEUE, CONFIG_MSG_COUNT_MAX)
+	COND_CODE_1(CONFIG_POSIX_MQUEUE, (CONFIG_MSG_COUNT_MAX), (0))
 #define _POSIX_MQ_PRIO_MAX                  (32)
 #define _POSIX_NAME_MAX                     (14)
 #define _POSIX_NGROUPS_MAX                  (8)
@@ -141,9 +223,12 @@
 #define _POSIX_SYMLINK_MAX                  (255)
 #define _POSIX_SYMLOOP_MAX                  (8)
 #define _POSIX_THREAD_DESTRUCTOR_ITERATIONS (4)
-#define _POSIX_THREAD_KEYS_MAX              (128)
-#define _POSIX_THREAD_THREADS_MAX           (64)
-#define _POSIX_TIMER_MAX                    (32)
+#define _POSIX_THREAD_KEYS_MAX \
+	COND_CODE_1(CONFIG_PTHREAD_IPC, (CONFIG_MAX_PTHREAD_KEY_COUNT), (0))
+#define _POSIX_THREAD_THREADS_MAX \
+	COND_CODE_1(CONFIG_PTHREAD_IPC, (CONFIG_MAX_PTHREAD_COUNT), (0))
+#define _POSIX_TIMER_MAX \
+	COND_CODE_1(CONFIG_POSIX_TIMERS, (CONFIG_POSIX_TIMER_MAX), (0))
 #define _POSIX_TRACE_EVENT_NAME_MAX         (30)
 #define _POSIX_TRACE_NAME_MAX               (8)
 #define _POSIX_TRACE_SYS_MAX                (8)
@@ -174,8 +259,7 @@
 #define AIO_MAX            _POSIX_AIO_MAX
 #define AIO_PRIO_DELTA_MAX (0)
 #define DELAYTIMER_MAX     _POSIX_DELAYTIMER_MAX
-#define HOST_NAME_MAX \
-	COND_CODE_1(CONFIG_NETWORKING, (NET_HOSTNAME_MAX_LEN), (_POSIX_HOST_NAME_MAX))
+#define HOST_NAME_MAX      _POSIX_HOST_NAME_MAX
 #define LOGIN_NAME_MAX     _POSIX_LOGIN_NAME_MAX
 #define MQ_OPEN_MAX        _POSIX_MQ_OPEN_MAX
 #define MQ_PRIO_MAX        _POSIX_MQ_PRIO_MAX
@@ -193,15 +277,15 @@
 #endif
 
 #define PTHREAD_DESTRUCTOR_ITERATIONS _POSIX_THREAD_DESTRUCTOR_ITERATIONS
-#define PTHREAD_KEYS_MAX                                                                           \
-	COND_CODE_1(CONFIG_PTHREAD_IPC, (CONFIG_MAX_PTHREAD_KEY_COUNT), (_POSIX_THREAD_KEYS_MAX))
-#define PTHREAD_THREADS_MAX COND_CODE_1(CONFIG_PTHREAD_IPC, (CONFIG_MAX_PTHREAD_COUNT), (0))
+#define PTHREAD_KEYS_MAX              _POSIX_THREAD_KEYS_MAX
+#define PTHREAD_THREADS_MAX           _POSIX_THREAD_THREADS_MAX
+#define RTSIG_MAX                     _POSIX_RTSIG_MAX
 #define SEM_NSEMS_MAX       _POSIX_SEM_NSEMS_MAX
 #define SEM_VALUE_MAX       CONFIG_SEM_VALUE_MAX
 #define SIGQUEUE_MAX        _POSIX_SIGQUEUE_MAX
 #define STREAM_MAX          _POSIX_STREAM_MAX
 #define SYMLOOP_MAX         _POSIX_SYMLOOP_MAX
-#define TIMER_MAX           Z_SC_VAL_IFDEF(CONFIG_TIMER, CONFIG_MAX_TIMER_COUNT)
+#define TIMER_MAX           _POSIX_TIMER_MAX
 #define TTY_NAME_MAX        _POSIX_TTY_NAME_MAX
 #define TZNAME_MAX          _POSIX_TZNAME_MAX
 

HoZHel/zephyr_ST

include/zephyr/posix/posix_features.h
@@ -11,101 +11,181 @@
 #include <autoconf.h>              /* CONFIG_* */
 #include <zephyr/sys/util_macro.h> /* COND_CODE_1() */
 
-/* Version test macros */
-#define _POSIX_VERSION  200809L
-#define _POSIX2_VERSION (-1L)
-#define _XOPEN_VERSION  (-1L)
+/*
+ * POSIX System Interfaces
+ */
 
-/* Internal helper macro to set constant if required Kconfig symbol is enabled */
-#define Z_SC_VAL_IFDEF(_def, _val) COND_CODE_1(_def, (_val), (-1L))
+#define _POSIX_VERSION 200809L
 
-/* Constants for Options and Option Groups */
-#define _POSIX_ADVISORY_INFO              (-1L)
-#define _POSIX_ASYNCHRONOUS_IO \
-	Z_SC_VAL_IFDEF(CONFIG_POSIX_ASYNCHRONOUS_IO, _POSIX_VERSION)
-#define _POSIX_BARRIERS                   Z_SC_VAL_IFDEF(CONFIG_PTHREAD_BARRIER, _POSIX_VERSION)
-#define _POSIX_CHOWN_RESTRICTED           (0)
-#define _POSIX_CLOCK_SELECTION            Z_SC_VAL_IFDEF(CONFIG_POSIX_CLOCK, _POSIX_VERSION)
-#define _POSIX_CPUTIME                    (-1L)
-#define _POSIX_FSYNC                      Z_SC_VAL_IFDEF(CONFIG_POSIX_FSYNC, _POSIX_VERSION)
-#define _POSIX_IPV6                       Z_SC_VAL_IFDEF(CONFIG_NET_IPV6, _POSIX_VERSION)
-#define _POSIX_JOB_CONTROL                (-1L)
-#define _POSIX_MAPPED_FILES               (-1L)
-#define _POSIX_MEMLOCK                    (-1L)
-#define _POSIX_MEMLOCK_RANGE              (-1L)
-#define _POSIX_MEMORY_PROTECTION          (-1L)
-#define _POSIX_MESSAGE_PASSING            Z_SC_VAL_IFDEF(CONFIG_POSIX_MQUEUE, _POSIX_VERSION)
-#define _POSIX_MONOTONIC_CLOCK            Z_SC_VAL_IFDEF(CONFIG_POSIX_CLOCK, _POSIX_VERSION)
-#define _POSIX_NO_TRUNC                   (0)
-#define _POSIX_PRIORITIZED_IO             (-1L)
-#define _POSIX_PRIORITY_SCHEDULING \
-	Z_SC_VAL_IFDEF(CONFIG_POSIX_PRIORITY_SCHEDULING, _POSIX_VERSION)
-#define _POSIX_RAW_SOCKETS                Z_SC_VAL_IFDEF(CONFIG_NET_SOCKETS_PACKET, _POSIX_VERSION)
-#define _POSIX_READER_WRITER_LOCKS        Z_SC_VAL_IFDEF(CONFIG_PTHREAD_IPC, _POSIX_VERSION)
-#define _POSIX_REALTIME_SIGNALS           (-1L)
-#define _POSIX_REGEXP                     (-1L)
-#define _POSIX_SAVED_IDS                  (-1L)
-#define _POSIX_SEMAPHORES                 Z_SC_VAL_IFDEF(CONFIG_PTHREAD_IPC, _POSIX_VERSION)
-#define _POSIX_SHARED_MEMORY_OBJECTS      (-1L)
-#define _POSIX_SHELL                      (-1L)
-#define _POSIX_SPAWN                      (-1L)
-#define _POSIX_SPIN_LOCKS                 Z_SC_VAL_IFDEF(CONFIG_PTHREAD_SPINLOCK, _POSIX_VERSION)
-#define _POSIX_SPORADIC_SERVER            (-1L)
-#define _POSIX_SYNCHRONIZED_IO            (-1L)
-#define _POSIX_THREAD_ATTR_STACKADDR      Z_SC_VAL_IFDEF(CONFIG_PTHREAD_IPC, _POSIX_VERSION)
-#define _POSIX_THREAD_ATTR_STACKSIZE      Z_SC_VAL_IFDEF(CONFIG_PTHREAD_IPC, _POSIX_VERSION)
-#define _POSIX_THREAD_CPUTIME             (-1L)
-#define _POSIX_THREAD_PRIO_INHERIT        _POSIX_VERSION
-#define _POSIX_THREAD_PRIO_PROTECT        (-1L)
-#define _POSIX_THREAD_PRIORITY_SCHEDULING Z_SC_VAL_IFDEF(CONFIG_PTHREAD_IPC, _POSIX_VERSION)
-#define _POSIX_THREAD_PROCESS_SHARED      (-1L)
-#define _POSIX_THREAD_ROBUST_PRIO_INHERIT (-1L)
-#define _POSIX_THREAD_ROBUST_PRIO_PROTECT (-1L)
-#define _POSIX_THREAD_SAFE_FUNCTIONS      Z_SC_VAL_IFDEF(CONFIG_PTHREAD_IPC, _POSIX_VERSION)
-#define _POSIX_THREAD_SPORADIC_SERVER     (-1L)
+#define _POSIX_CHOWN_RESTRICTED (0)
+#define _POSIX_NO_TRUNC         (0)
+#define _POSIX_VDISABLE         ('\0')
 
-#ifndef _POSIX_THREADS
-#define _POSIX_THREADS Z_SC_VAL_IFDEF(CONFIG_PTHREAD_IPC, _POSIX_VERSION)
+/* #define _POSIX_ADVISORY_INFO (-1L) */
+
+#ifdef CONFIG_POSIX_ASYNCHRONOUS_IO
+#define _POSIX_ASYNCHRONOUS_IO _POSIX_VERSION
 #endif
 
-#define _POSIX_TIMEOUTS             Z_SC_VAL_IFDEF(CONFIG_POSIX_CLOCK, _POSIX_VERSION)
-#define _POSIX_TIMERS               Z_SC_VAL_IFDEF(CONFIG_POSIX_CLOCK, _POSIX_VERSION)
-#define _POSIX_TRACE                (-1L)
-#define _POSIX_TRACE_EVENT_FILTER   (-1L)
-#define _POSIX_TRACE_INHERIT        (-1L)
-#define _POSIX_TRACE_LOG            (-1L)
-#define _POSIX_TYPED_MEMORY_OBJECTS (-1L)
-#define _POSIX_V6_ILP32_OFF32       (-1L)
-#define _POSIX_V6_ILP32_OFFBIG      (-1L)
-#define _POSIX_V6_LP64_OFF64        (-1L)
-#define _POSIX_V6_LPBIG_OFFBIG      (-1L)
-#define _POSIX_V7_ILP32_OFF32       (-1L)
-#define _POSIX_V7_ILP32_OFFBIG      (-1L)
-#define _POSIX_V7_LP64_OFF64        (-1L)
-#define _POSIX_V7_LPBIG_OFFBIG      (-1L)
-#define _POSIX2_C_BIND              _POSIX_VERSION
-#define _POSIX2_C_DEV               (-1L)
-#define _POSIX2_CHAR_TERM           (-1L)
-#define _POSIX2_DELAYTIMER_MAX      (-1L)
-#define _POSIX2_FORT_DEV            (-1L)
-#define _POSIX2_FORT_RUN            (-1L)
-#define _POSIX2_LOCALEDEF           (-1L)
-#define _POSIX2_PBS                 (-1L)
-#define _POSIX2_PBS_ACCOUNTING      (-1L)
-#define _POSIX2_PBS_CHECKPOINT      (-1L)
-#define _POSIX2_PBS_LOCATE          (-1L)
-#define _POSIX2_PBS_MESSAGE         (-1L)
-#define _POSIX2_PBS_TRACK           (-1L)
-#define _POSIX2_SW_DEV              (-1L)
-#define _POSIX2_UPE                 (-1L)
-#define _XOPEN_CRYPT                (-1L)
-#define _XOPEN_ENH_I18N             (-1L)
-#define _XOPEN_REALTIME             (-1L)
-#define _XOPEN_REALTIME_THREADS     (-1L)
-#define _XOPEN_SHM                  (-1L)
-#define _XOPEN_STREAMS              (-1L)
-#define _XOPEN_UNIX                 (-1L)
-#define _XOPEN_UUCP                 (-1L)
+#ifdef CONFIG_PTHREAD_BARRIER
+#define _POSIX_BARRIERS _POSIX_VERSION
+#endif
+
+#ifdef CONFIG_POSIX_CLOCK
+#define _POSIX_CLOCK_SELECTION _POSIX_VERSION
+#endif
+
+/* #define _POSIX_CPUTIME (-1L) */
+
+#ifdef CONFIG_POSIX_FSYNC
+#define _POSIX_FSYNC _POSIX_VERSION
+#endif
+
+#ifdef CONFIG_NET_IPV6
+#define _POSIX_IPV6 _POSIX_VERSION
+#endif
+
+/* #define _POSIX_JOB_CONTROL (-1L) */
+/* #define _POSIX_MAPPED_FILES (-1L) */
+/* #define _POSIX_MEMLOCK (-1L) */
+/* #define _POSIX_MEMLOCK_RANGE (-1L) */
+/* #define _POSIX_MEMORY_PROTECTION (-1L) */
+
+#ifdef CONFIG_POSIX_MQUEUE
+#define _POSIX_MESSAGE_PASSING _POSIX_VERSION
+#endif
+
+#ifdef CONFIG_POSIX_CLOCK
+#define _POSIX_MONOTONIC_CLOCK _POSIX_VERSION
+#endif
+
+/* #define _POSIX_PRIORITIZED_IO (-1L) */
+
+#ifdef CONFIG_POSIX_PRIORITY_SCHEDULING
+#define _POSIX_PRIORITY_SCHEDULING _POSIX_VERSION
+#endif
+
+#ifdef CONFIG_NET_SOCKETS_PACKET
+#define _POSIX_RAW_SOCKETS _POSIX_VERSION
+#endif
+
+#ifdef CONFIG_PTHREAD_IPC
+#define _POSIX_READER_WRITER_LOCKS _POSIX_VERSION
+#endif
+
+/* #define _POSIX_REALTIME_SIGNALS (-1L) */
+/* #define _POSIX_REGEXP (-1L) */
+/* #define _POSIX_SAVED_IDS (-1L) */
+
+#ifdef CONFIG_PTHREAD_IPC
+#define _POSIX_SEMAPHORES _POSIX_VERSION
+#endif
+
+/* #define _POSIX_SHARED_MEMORY_OBJECTS (-1L) */
+/* #define _POSIX_SHELL (-1L) */
+/* #define _POSIX_SPAWN (-1L) */
+
+#ifdef CONFIG_PTHREAD_SPINLOCK
+#define _POSIX_SPIN_LOCKS _POSIX_VERSION
+#endif
+
+/* #define _POSIX_SPORADIC_SERVER (-1L) */
+/* #define _POSIX_SYNCHRONIZED_IO (-1L) */
+
+#ifdef CONFIG_PTHREAD_IPC
+#define _POSIX_THREAD_ATTR_STACKADDR _POSIX_VERSION
+#endif
+
+#ifdef CONFIG_PTHREAD_IPC
+#define _POSIX_THREAD_ATTR_STACKSIZE _POSIX_VERSION
+#endif
+
+/* #define _POSIX_THREAD_CPUTIME		  (-1L) */
+
+#ifdef CONFIG_PTHREAD_IPC
+#define _POSIX_THREAD_PRIO_INHERIT _POSIX_VERSION
+#endif
+
+/* #define _POSIX_THREAD_PRIO_PROTECT (-1L) */
+/* #define _POSIX_THREAD_PRIORITY_SCHEDULING (-1L) */
+/* #define _POSIX_THREAD_PROCESS_SHARED (-1L) */
+/* #define _POSIX_THREAD_ROBUST_PRIO_INHERIT (-1L) */
+/* #define _POSIX_THREAD_ROBUST_PRIO_PROTECT (-1L) */
+
+#ifdef CONFIG_PTHREAD_IPC
+#define _POSIX_THREAD_SAFE_FUNCTIONS _POSIX_VERSION
+#endif
+
+/* #define _POSIX_THREAD_SPORADIC_SERVER (-1L) */
+
+#ifdef CONFIG_PTHREAD_IPC
+#ifndef _POSIX_THREADS
+#define _POSIX_THREADS _POSIX_VERSION
+#endif
+#endif
+
+#ifdef CONFIG_POSIX_CLOCK
+#define _POSIX_TIMEOUTS _POSIX_VERSION
+#endif
+
+#ifdef CONFIG_POSIX_CLOCK
+#define _POSIX_TIMERS _POSIX_VERSION
+#endif
+
+/* #define _POSIX_TRACE (-1L) */
+/* #define _POSIX_TRACE_EVENT_FILTER (-1L) */
+/* #define _POSIX_TRACE_INHERIT (-1L) */
+/* #define _POSIX_TRACE_LOG (-1L) */
+/* #define _POSIX_TYPED_MEMORY_OBJECTS (-1L) */
+
+/*
+ * POSIX v6 Options
+ */
+/* #define _POSIX_V6_ILP32_OFF32 (-1L) */
+/* #define _POSIX_V6_ILP32_OFFBIG (-1L) */
+/* #define _POSIX_V6_LP64_OFF64 (-1L) */
+/* #define _POSIX_V6_LPBIG_OFFBIG (-1L) */
+
+/*
+ * POSIX v7 Options
+ */
+/* #define _POSIX_V7_ILP32_OFF32 (-1L) */
+/* #define _POSIX_V7_ILP32_OFFBIG (-1L) */
+/* #define _POSIX_V7_LP64_OFF64 (-1L) */
+/* #define _POSIX_V7_LPBIG_OFFBIG (-1L) */
+
+/*
+ * POSIX2 Options
+ */
+#define _POSIX2_VERSION _POSIX_VERSION
+#define _POSIX2_C_BIND  _POSIX2_VERSION
+#define _POSIX2_C_DEV   _POSIX2_VERSION
+/* #define _POSIX2_CHAR_TERM (-1L) */
+/* #define _POSIX2_DELAYTIMER_MAX (-1L) */
+/* #define _POSIX2_FORT_DEV (-1L) */
+/* #define _POSIX2_FORT_RUN (-1L) */
+/* #define _POSIX2_LOCALEDEF (-1L) */
+/* #define _POSIX2_PBS (-1L) */
+/* #define _POSIX2_PBS_ACCOUNTING (-1L) */
+/* #define _POSIX2_PBS_CHECKPOINT (-1L) */
+/* #define _POSIX2_PBS_LOCATE (-1L) */
+/* #define _POSIX2_PBS_MESSAGE (-1L) */
+/* #define _POSIX2_PBS_TRACK (-1L) */
+/* #define _POSIX2_SW_DEV (-1L) */
+/* #define _POSIX2_UPE (-1L) */
+
+/*
+ * X/Open System Interfaces
+ */
+#define _XOPEN_VERSION 700
+/* #define _XOPEN_CRYPT (-1L) */
+/* #define _XOPEN_ENH_I18N (-1L) */
+/* #define _XOPEN_REALTIME (-1L) */
+/* #define _XOPEN_REALTIME_THREADS (-1L) */
+/* #define _XOPEN_SHM (-1L) */
+/* #define _XOPEN_STREAMS (-1L) */
+/* #define _XOPEN_UNIX (-1L) */
+/* #define _XOPEN_UUCP (-1L) */
 
 /* Maximum values */
 #define _POSIX_CLOCKRES_MIN (20000000L)
@@ -115,14 +195,16 @@
 #define _POSIX_AIO_MAX                      (1)
 #define _POSIX_ARG_MAX                      (4096)
 #define _POSIX_CHILD_MAX                    (25)
-#define _POSIX_DELAYTIMER_MAX               (32)
-#define _POSIX_HOST_NAME_MAX                (255)
+#define _POSIX_DELAYTIMER_MAX \
+	COND_CODE_1(CONFIG_POSIX_TIMERS, (CONFIG_POSIX_DELAYTIMER_MAX), (0))
+#define _POSIX_HOST_NAME_MAX \
+	COND_CODE_1(CONFIG_NET_HOSTNAME_MAX_LEN, (NET_HOSTNAME_MAX_LEN), (0))
 #define _POSIX_LINK_MAX                     (8)
 #define _POSIX_LOGIN_NAME_MAX               (9)
 #define _POSIX_MAX_CANON                    (255)
 #define _POSIX_MAX_INPUT                    (255)
 #define _POSIX_MQ_OPEN_MAX \
-	Z_SC_VAL_IFDEF(CONFIG_POSIX_MQUEUE, CONFIG_MSG_COUNT_MAX)
+	COND_CODE_1(CONFIG_POSIX_MQUEUE, (CONFIG_MSG_COUNT_MAX), (0))
 #define _POSIX_MQ_PRIO_MAX                  (32)
 #define _POSIX_NAME_MAX                     (14)
 #define _POSIX_NGROUPS_MAX                  (8)
@@ -141,9 +223,12 @@
 #define _POSIX_SYMLINK_MAX                  (255)
 #define _POSIX_SYMLOOP_MAX                  (8)
 #define _POSIX_THREAD_DESTRUCTOR_ITERATIONS (4)
-#define _POSIX_THREAD_KEYS_MAX              (128)
-#define _POSIX_THREAD_THREADS_MAX           (64)
-#define _POSIX_TIMER_MAX                    (32)
+#define _POSIX_THREAD_KEYS_MAX \
+	COND_CODE_1(CONFIG_PTHREAD_IPC, (CONFIG_MAX_PTHREAD_KEY_COUNT), (0))
+#define _POSIX_THREAD_THREADS_MAX \
+	COND_CODE_1(CONFIG_PTHREAD_IPC, (CONFIG_MAX_PTHREAD_COUNT), (0))
+#define _POSIX_TIMER_MAX \
+	COND_CODE_1(CONFIG_POSIX_TIMERS, (CONFIG_POSIX_TIMER_MAX), (0))
 #define _POSIX_TRACE_EVENT_NAME_MAX         (30)
 #define _POSIX_TRACE_NAME_MAX               (8)
 #define _POSIX_TRACE_SYS_MAX                (8)
@@ -174,8 +259,7 @@
 #define AIO_MAX            _POSIX_AIO_MAX
 #define AIO_PRIO_DELTA_MAX (0)
 #define DELAYTIMER_MAX     _POSIX_DELAYTIMER_MAX
-#define HOST_NAME_MAX \
-	COND_CODE_1(CONFIG_NETWORKING, (NET_HOSTNAME_MAX_LEN), (_POSIX_HOST_NAME_MAX))
+#define HOST_NAME_MAX      _POSIX_HOST_NAME_MAX
 #define LOGIN_NAME_MAX     _POSIX_LOGIN_NAME_MAX
 #define MQ_OPEN_MAX        _POSIX_MQ_OPEN_MAX
 #define MQ_PRIO_MAX        _POSIX_MQ_PRIO_MAX
@@ -193,15 +277,15 @@
 #endif
 
 #define PTHREAD_DESTRUCTOR_ITERATIONS _POSIX_THREAD_DESTRUCTOR_ITERATIONS
-#define PTHREAD_KEYS_MAX                                                                           \
-	COND_CODE_1(CONFIG_PTHREAD_IPC, (CONFIG_MAX_PTHREAD_KEY_COUNT), (_POSIX_THREAD_KEYS_MAX))
-#define PTHREAD_THREADS_MAX COND_CODE_1(CONFIG_PTHREAD_IPC, (CONFIG_MAX_PTHREAD_COUNT), (0))
+#define PTHREAD_KEYS_MAX              _POSIX_THREAD_KEYS_MAX
+#define PTHREAD_THREADS_MAX           _POSIX_THREAD_THREADS_MAX
+#define RTSIG_MAX                     _POSIX_RTSIG_MAX
 #define SEM_NSEMS_MAX       _POSIX_SEM_NSEMS_MAX
 #define SEM_VALUE_MAX       CONFIG_SEM_VALUE_MAX
 #define SIGQUEUE_MAX        _POSIX_SIGQUEUE_MAX
 #define STREAM_MAX          _POSIX_STREAM_MAX
 #define SYMLOOP_MAX         _POSIX_SYMLOOP_MAX
-#define TIMER_MAX           Z_SC_VAL_IFDEF(CONFIG_TIMER, CONFIG_MAX_TIMER_COUNT)
+#define TIMER_MAX           _POSIX_TIMER_MAX
 #define TTY_NAME_MAX        _POSIX_TTY_NAME_MAX
 #define TZNAME_MAX          _POSIX_TZNAME_MAX
 