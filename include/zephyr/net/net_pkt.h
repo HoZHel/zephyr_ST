awojasinski-nordicsemi/zephyr

net: pkt: Add flags to indicate the pkt should be timestamped
This is the first commit from the set of patches that brings to the
Zephyr, SO_TIMESTAMPING socket level option. This enables to pass to
the network driver information whether given network packet should
be timestamped or not.

Signed-off-by: Adam Wojasinski <awojasinski@baylibre.com>

include/zephyr/net/net_pkt.h
@@ -115,6 +115,8 @@ struct net_pkt {
 	 * TODO: Replace with net_time_t to decouple from PTP.
 	 */
 	struct net_ptp_time timestamp;
+	uint8_t tx_timestamping : 1; /** Timestamp transfered packet */
+	uint8_t rx_timestamping : 1; /** Timestamp received packet */
 #endif
 
 #if defined(CONFIG_NET_PKT_RXTIME_STATS) || defined(CONFIG_NET_PKT_TXTIME_STATS)
@@ -384,6 +386,28 @@ static inline void net_pkt_set_ptp(struct net_pkt *pkt, bool is_ptp)
 {
 	pkt->ptp_pkt = is_ptp;
 }
+
+#if defined(CONFIG_NET_PKT_TIMESTAMP)
+static inline bool net_pkt_is_tx_timestamping(struct net_pkt *pkt)
+{
+	return !!(pkt->tx_timestamping);
+}
+
+static inline void net_pkt_set_tx_timestamping(struct net_pkt *pkt, bool is_timestamping)
+{
+	pkt->tx_timestamping = is_timestamping;
+}
+
+static inline bool net_pkt_is_rx_timestamping(struct net_pkt *pkt)
+{
+	return !!(pkt->rx_timestamping);
+}
+
+static inline void net_pkt_set_rx_timestamping(struct net_pkt *pkt, bool is_timestamping)
+{
+	pkt->rx_timestamping = is_timestamping;
+}
+#endif
 
 static inline bool net_pkt_is_captured(struct net_pkt *pkt)
 {

HoZHel/zephyr_ST

include/zephyr/net/net_pkt.h
@@ -198,6 +198,10 @@ struct net_pkt {
 #if defined(CONFIG_NET_IP_FRAGMENT)
 	uint8_t ip_reassembled : 1; /* Packet is a reassembled IP packet. */
 #endif
+#if defined(CONFIG_NET_PKT_TIMESTAMP)
+	uint8_t tx_timestamping : 1; /** Timestamp transmitted packet */
+	uint8_t rx_timestamping : 1; /** Timestamp received packet */
+#endif
 	/* bitfield byte alignment boundary */
 
 #if defined(CONFIG_NET_IP)
@@ -387,6 +391,38 @@ static inline bool net_pkt_is_ptp(struct net_pkt *pkt)
 static inline void net_pkt_set_ptp(struct net_pkt *pkt, bool is_ptp)
 {
 	pkt->ptp_pkt = is_ptp;
+}
+
+static inline bool net_pkt_is_tx_timestamping(struct net_pkt *pkt)
+{
+#if defined(CONFIG_NET_PKT_TIMESTAMP)
+	return !!(pkt->tx_timestamping);
+#else
+	return false;
+#endif
+}
+
+static inline void net_pkt_set_tx_timestamping(struct net_pkt *pkt, bool is_timestamping)
+{
+#if defined(CONFIG_NET_PKT_TIMESTAMP)
+	pkt->tx_timestamping = is_timestamping;
+#endif
+}
+
+static inline bool net_pkt_is_rx_timestamping(struct net_pkt *pkt)
+{
+#if defined(CONFIG_NET_PKT_TIMESTAMP)
+	return !!(pkt->rx_timestamping);
+#else
+	return false;
+#endif
+}
+
+static inline void net_pkt_set_rx_timestamping(struct net_pkt *pkt, bool is_timestamping)
+{
+#if defined(CONFIG_NET_PKT_TIMESTAMP)
+	pkt->rx_timestamping = is_timestamping;
+#endif
 }
 
 static inline bool net_pkt_is_captured(struct net_pkt *pkt)