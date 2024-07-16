dztang/zephyr

Bluetooth: A2DP: Implement the a2dp and avdtp
implement a2dp.c and avdtp.c
add a2dp related Kconfig:
BT_AVDTP_RTP_VERSION, BT_A2DP_SOURCE and BT_A2DP_SINK
a2dp_codec_sbc.c/h are used to provide some APIs to get
A2DP SBC codec information. (like: channel num).

Signed-off-by: Mark Wang <yichang.wang@nxp.com>

include/zephyr/bluetooth/classic/avdtp.h
@@ -4,49 +4,140 @@
 
 /*
  * Copyright (c) 2015-2016 Intel Corporation
+ * Copyright 2024 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 #ifndef ZEPHYR_INCLUDE_BLUETOOTH_AVDTP_H_
 #define ZEPHYR_INCLUDE_BLUETOOTH_AVDTP_H_
 
-#include <zephyr/bluetooth/l2cap.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/** @brief AVDTP SEID Information */
-struct bt_avdtp_seid_info {
-	/** Stream End Point ID */
-	uint8_t id:6;
-	/** End Point usage status */
-	uint8_t inuse:1;
-	/** Reserved */
-	uint8_t rfa0:1;
-	/** Media-type of the End Point */
-	uint8_t media_type:4;
-	/** TSEP of the End Point */
-	uint8_t tsep:1;
-	/** Reserved */
-	uint8_t rfa1:3;
-} __packed;
+#define BAD_HEADER_FORMAT 0x01
+#define BAD_ACP_SEID 0x12
+#define INVALID_CAPABILITIES 0x1A
+#define BAD_STATE 0x31
 
-/** @brief AVDTP Local SEP*/
-struct bt_avdtp_seid_lsep {
-	/** Stream End Point information */
-	struct bt_avdtp_seid_info sep;
-	/** Pointer to next local Stream End Point structure */
-	struct bt_avdtp_seid_lsep *next;
+/**
+ * @brief AVDTP error code
+ */
+enum bt_avdtp_err_code {
+	/** The response is success, it is not from avdtp spec. */
+	BT_AVDTP_SUCCESS = 0x00,
+	/** The request is time out without response, it is not from avdtp spec. */
+	BT_AVDTP_TIME_OUT = 0xFF,
+	/** The request packet header format error */
+	BT_AVDTP_BAD_HEADER_FORMAT = 0x01,
+	/** The request packet length is not match the assumed length */
+	BT_AVDTP_BAD_LENGTH = 0x11,
+	/** The requested command indicates an invalid ACP SEID (not addressable) */
+	BT_AVDTP_BAD_ACP_SEID = 0x12,
+	/** The SEP is in use */
+	BT_AVDTP_SEP_IN_USE = 0x13,
+	/** The SEP is not in use */
+	BT_AVDTP_SEP_NOT_IN_USE = 0x14,
+	/** The value of Service Category in the request packet is not defined in AVDTP */
+	BT_AVDTP_BAD_SERV_CATEGORY = 0x17,
+	/** The requested command has an incorrect payload format */
+	BT_AVDTP_BAD_PAYLOAD_FORMAT = 0x18,
+	/** The requested command is not supported by the device */
+	BT_AVDTP_NOT_SUPPORTED_COMMAND = 0x19,
+	/** The reconfigure command is an attempt to reconfigure a transport service codec_cap
+	 * of the SEP. Reconfigure is only permitted for application service codec_cap
+	 */
+	BT_AVDTP_INVALID_CAPABILITIES = 0x1A,
+	/** The requested Recovery Type is not defined in AVDTP */
+	BT_AVDTP_BAD_RECOVERY_TYPE = 0x22,
+	/** The format of Media Transport Capability is not correct */
+	BT_AVDTP_BAD_MEDIA_TRANSPORT_FORMAT = 0x23,
+	/** The format of Recovery Service Capability is not correct */
+	BT_AVDTP_BAD_RECOVERY_FORMAT = 0x25,
+	/** The format of Header Compression Service Capability is not correct */
+	BT_AVDTP_BAD_ROHC_FORMAT = 0x26,
+	/** The format of Content Protection Service Capability is not correct */
+	BT_AVDTP_BAD_CP_FORMAT = 0x27,
+	/** The format of Multiplexing Service Capability is not correct */
+	BT_AVDTP_BAD_MULTIPLEXING_FORMAT = 0x28,
+	/** Configuration not supported */
+	BT_AVDTP_UNSUPPORTED_CONFIGURAION = 0x29,
+	/** Indicates that the ACP state machine is in an invalid state in order to process the
+	 * signal. This also includes the situation when an INT receives a request for the
+	 * same command that it is currently expecting a response
+	 */
+	BT_AVDTP_BAD_STATE = 0x31,
 };
 
-/** @brief AVDTP Stream */
-struct bt_avdtp_stream {
-	struct bt_l2cap_br_chan chan; /* Transport Channel*/
-	struct bt_avdtp_seid_info lsep; /* Configured Local SEP */
-	struct bt_avdtp_seid_info rsep; /* Configured Remote SEP*/
-	uint8_t state; /* current state of the stream */
-	struct bt_avdtp_stream *next;
+/** @brief Stream End Point Role */
+enum ROLE_TYPE {
+	/** Source Role */
+	BT_AVDTP_SOURCE = 0,
+	/** Sink Role */
+	BT_AVDTP_SINK = 1
+};
+
+/** @brief Stream End Point Media Type */
+enum MEDIA_TYPE {
+	/** Audio Media Type */
+	BT_AVDTP_AUDIO = 0x00,
+	/** Video Media Type */
+	BT_AVDTP_VIDEO = 0x01,
+	/** Multimedia Media Type */
+	BT_AVDTP_MULTIMEDIA = 0x02
+};
+
+/** @brief AVDTP SEID Information */
+struct bt_avdtp_seid_info {
+	/** Reserved */
+	uint8_t rfa0:1;
+	/** End Point usage status */
+	uint8_t inuse:1;
+	/** Stream End Point ID */
+	uint8_t id:6;
+	/** Reserved */
+	uint8_t rfa1:3;
+	/** TSEP of the End Point */
+	uint8_t tsep:1;
+	/** Media-type of the End Point */
+	uint8_t media_type:4;
+} __packed;
+
+/** @brief service category Type */
+enum SERVICE_CATEGORY {
+	/** Media Transport */
+	BT_AVDTP_SERVICE_MEDIA_TRANSPORT = 0x01,
+	/** Reporting */
+	BT_AVDTP_SERVICE_REPORTING = 0x02,
+	/** Recovery */
+	BT_AVDTP_SERVICE_MEDIA_RECOVERY = 0x03,
+	/** Content Protection */
+	BT_AVDTP_SERVICE_CONTENT_PROTECTION = 0x04,
+	/** Header Compression */
+	BT_AVDTP_SERVICE_HEADER_COMPRESSION = 0x05,
+	/** Multiplexing */
+	BT_AVDTP_SERVICE_MULTIPLEXING = 0x06,
+	/** Media Codec */
+	BT_AVDTP_SERVICE_MEDIA_CODEC = 0x07,
+	/** Delay Reporting */
+	BT_AVDTP_SERVICE_DELAY_REPORTING = 0x08,
+};
+
+/** @brief AVDTP Stream End Point Identifier */
+struct bt_avdtp_seid {
+	/** Stream End Point information */
+	struct bt_avdtp_seid_info sep;
+	/** Transport Channel*/
+	struct bt_l2cap_br_chan media_br_chan;
+	/** the endpoint media data */
+	void (*media_data_cb)(struct bt_avdtp_seid *seid,
+				struct net_buf *buf);
+	/** avdtp session */
+	struct bt_avdtp *session;
+	/** SEP state */
+	uint8_t seid_state;
+	/* Internally used list node */
+	sys_snode_t _node;
 };
 
 #ifdef __cplusplus

subsys/bluetooth/host/classic/avdtp_internal.h
@@ -2,6 +2,7 @@
  * avdtp_internal.h - avdtp handling
 
  * Copyright (c) 2015-2016 Intel Corporation
+ * Copyright 2021,2024 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -82,22 +83,19 @@
 #define BT_AVDTP_ERR_UNSUPPORTED_CONFIGURAION   0x29
 #define BT_AVDTP_ERR_BAD_STATE                  0x31
 
-#define BT_AVDTP_MAX_MTU BT_L2CAP_RX_MTU
-
 #define BT_AVDTP_MIN_SEID 0x01
 #define BT_AVDTP_MAX_SEID 0x3E
 
 struct bt_avdtp;
 struct bt_avdtp_req;
+struct bt_avdtp_seid_info;
 
-typedef int (*bt_avdtp_func_t)(struct bt_avdtp *session,
-			       struct bt_avdtp_req *req);
+typedef int (*bt_avdtp_func_t)(struct bt_avdtp_req *req);
 
 struct bt_avdtp_req {
 	uint8_t sig;
 	uint8_t tid;
 	bt_avdtp_func_t func;
-	struct k_work_delayable timeout_work;
 };
 
 struct bt_avdtp_single_sig_hdr {
@@ -105,47 +103,99 @@ struct bt_avdtp_single_sig_hdr {
 	uint8_t signal_id;
 } __packed;
 
-#define BT_AVDTP_SIG_HDR_LEN sizeof(struct bt_avdtp_single_sig_hdr)
-
-struct bt_avdtp_ind_cb {
-	/*
-	 * discovery_ind;
-	 * get_capabilities_ind;
-	 * set_configuration_ind;
-	 * open_ind;
-	 * start_ind;
-	 * suspend_ind;
-	 * close_ind;
-	 */
-};
-
-struct bt_avdtp_cap {
-	uint8_t cat;
-	uint8_t len;
-	uint8_t data[0];
-};
-
-struct bt_avdtp_sep {
-	uint8_t seid;
-	uint8_t len;
-	struct bt_avdtp_cap caps[0];
-};
+struct bt_avdtp_media_hdr {
+	uint8_t CSRC_count:4;
+	uint8_t header_extension:1;
+	uint8_t padding:1;
+	uint8_t RTP_version:2;
+	uint8_t playload_type:7;
+	uint8_t marker:1;
+	uint16_t sequence_number;
+	uint32_t time_stamp;
+	uint32_t synchronization_source;
+} __packed;
 
 struct bt_avdtp_discover_params {
 	struct bt_avdtp_req req;
 	uint8_t status;
-	struct bt_avdtp_sep *caps;
+	struct net_buf *buf;
+};
+
+struct bt_avdtp_get_capabilities_params {
+	struct bt_avdtp_req req;
+	uint8_t status;
+	uint8_t stream_endpoint_id;
+	struct net_buf *buf;
+};
+
+struct bt_avdtp_set_configuration_params {
+	struct bt_avdtp_req req;
+	struct bt_avdtp_seid *seid;
+	uint8_t status;
+	uint8_t acp_stream_ep_id;
+	uint8_t int_stream_endpoint_id;
+	uint8_t media_type;
+	uint8_t media_codec_type;
+	uint8_t codec_specific_ie_len;
+	uint8_t *codec_specific_ie;
+};
+
+struct bt_avdtp_open_params {
+	struct bt_avdtp_req req;
+	struct bt_avdtp_seid *seid;
+	uint8_t status;
+	uint8_t acp_stream_ep_id;
+};
+
+struct bt_avdtp_start_params {
+	struct bt_avdtp_req req;
+	struct bt_avdtp_seid *seid;
+	uint8_t status;
+	uint8_t acp_stream_ep_id;
+};
+
+struct bt_avdtp_ops_cb {
+	void (*connected)(struct bt_avdtp *session);
+
+	void (*disconnected)(struct bt_avdtp *session);
+
+	struct net_buf *(*alloc_buf)(struct bt_avdtp *session);
+
+	int (*discovery_ind)(struct bt_avdtp *session, uint8_t *errcode);
+
+	int (*get_capabilities_ind)(struct bt_avdtp *session,
+		struct bt_avdtp_seid *seid, struct net_buf *rsp_buf, uint8_t *errcode);
+
+	int (*set_configuration_ind)(struct bt_avdtp *session, struct bt_avdtp_seid *seid,
+		uint8_t int_seid, struct net_buf *buf, uint8_t *errcode);
+
+	int (*open_ind)(struct bt_avdtp *session,
+		struct bt_avdtp_seid *seid, uint8_t *errcode);
+
+	int (*close_ind)(struct bt_avdtp *session,
+		struct bt_avdtp_seid *seid, uint8_t *errcode);
+
+	int (*start_ind)(struct bt_avdtp *session,
+		struct bt_avdtp_seid *seid, uint8_t *errcode);
+
+	int (*suspend_ind)(struct bt_avdtp *session,
+		struct bt_avdtp_seid *seid, uint8_t *errcode);
+
+	int (*abort_ind)(struct bt_avdtp *session,
+		struct bt_avdtp_seid *seid, uint8_t *errcode);
 };
 
 /** @brief Global AVDTP session structure. */
 struct bt_avdtp {
 	struct bt_l2cap_br_chan br_chan;
-	struct bt_avdtp_stream *streams; /* List of AV streams */
 	struct bt_avdtp_req *req;
+	const struct bt_avdtp_ops_cb *ops;
+	struct bt_avdtp_seid *current_seid;
+	struct k_work_delayable timeout_work;
+	uint8_t signalling_l2cap_connected;
 };
 
 struct bt_avdtp_event_cb {
-	struct bt_avdtp_ind_cb *ind;
 	int (*accept)(struct bt_conn *conn, struct bt_avdtp **session);
 };
 
@@ -163,8 +213,41 @@ int bt_avdtp_disconnect(struct bt_avdtp *session);
 
 /* AVDTP SEP register function */
 int bt_avdtp_register_sep(uint8_t media_type, uint8_t role,
-				struct bt_avdtp_seid_lsep *sep);
+				struct bt_avdtp_seid *sep);
 
 /* AVDTP Discover Request */
 int bt_avdtp_discover(struct bt_avdtp *session,
 		      struct bt_avdtp_discover_params *param);
+
+/* Parse the seid of discovered result */
+struct bt_avdtp_seid_info *bt_avdtp_parse_seids(struct net_buf *buf);
+
+/* AVDTP Get Capabilities */
+int bt_avdtp_get_capabilities(struct bt_avdtp *session,
+		      struct bt_avdtp_get_capabilities_params *param);
+
+/* Parse the codec type of capabilities */
+int bt_avdtp_parse_capability_codec(struct net_buf *buf, uint8_t *codec_type,
+				uint8_t **codec_info_element, uint16_t *codec_info_element_len);
+
+/* AVDTP Set Configuration */
+int bt_avdtp_set_configuration(struct bt_avdtp *session,
+		      struct bt_avdtp_set_configuration_params *param);
+
+/* AVDTP reconfigure */
+int bt_avdtp_reconfigure(struct bt_avdtp *session,
+		      struct bt_avdtp_set_configuration_params *param);
+
+/* AVDTP OPEN */
+int bt_avdtp_open(struct bt_avdtp *session,
+		      struct bt_avdtp_open_params *param);
+
+/* AVDTP START */
+int bt_avdtp_start(struct bt_avdtp *session,
+		      struct bt_avdtp_start_params *param);
+
+/* AVDTP send data */
+int bt_avdtp_send_media_data(struct bt_avdtp_seid *seid, struct net_buf *buf);
+
+/* get media l2cap connection MTU */
+uint32_t bt_avdtp_get_media_mtu(struct bt_avdtp_seid *seid);

HoZHel/zephyr_ST

include/zephyr/bluetooth/classic/avdtp.h
@@ -4,49 +4,137 @@
 
 /*
  * Copyright (c) 2015-2016 Intel Corporation
+ * Copyright 2024 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 #ifndef ZEPHYR_INCLUDE_BLUETOOTH_AVDTP_H_
 #define ZEPHYR_INCLUDE_BLUETOOTH_AVDTP_H_
 
-#include <zephyr/bluetooth/l2cap.h>
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/** @brief AVDTP SEID Information */
-struct bt_avdtp_seid_info {
-	/** Stream End Point ID */
-	uint8_t id:6;
-	/** End Point usage status */
-	uint8_t inuse:1;
-	/** Reserved */
-	uint8_t rfa0:1;
-	/** Media-type of the End Point */
-	uint8_t media_type:4;
-	/** TSEP of the End Point */
-	uint8_t tsep:1;
-	/** Reserved */
-	uint8_t rfa1:3;
-} __packed;
-
-/** @brief AVDTP Local SEP*/
-struct bt_avdtp_seid_lsep {
-	/** Stream End Point information */
-	struct bt_avdtp_seid_info sep;
-	/** Pointer to next local Stream End Point structure */
-	struct bt_avdtp_seid_lsep *next;
+/**
+ * @brief AVDTP error code
+ */
+enum bt_avdtp_err_code {
+	/** The response is success, it is not from avdtp spec. */
+	BT_AVDTP_SUCCESS = 0x00,
+	/** The request is time out without response, it is not from avdtp spec. */
+	BT_AVDTP_TIME_OUT = 0xFF,
+	/** The request packet header format error */
+	BT_AVDTP_BAD_HEADER_FORMAT = 0x01,
+	/** The request packet length is not match the assumed length */
+	BT_AVDTP_BAD_LENGTH = 0x11,
+	/** The requested command indicates an invalid ACP SEID (not addressable) */
+	BT_AVDTP_BAD_ACP_SEID = 0x12,
+	/** The SEP is in use */
+	BT_AVDTP_SEP_IN_USE = 0x13,
+	/** The SEP is not in use */
+	BT_AVDTP_SEP_NOT_IN_USE = 0x14,
+	/** The value of Service Category in the request packet is not defined in AVDTP */
+	BT_AVDTP_BAD_SERV_CATEGORY = 0x17,
+	/** The requested command has an incorrect payload format */
+	BT_AVDTP_BAD_PAYLOAD_FORMAT = 0x18,
+	/** The requested command is not supported by the device */
+	BT_AVDTP_NOT_SUPPORTED_COMMAND = 0x19,
+	/** The reconfigure command is an attempt to reconfigure a transport service codec_cap
+	 * of the SEP. Reconfigure is only permitted for application service codec_cap
+	 */
+	BT_AVDTP_INVALID_CAPABILITIES = 0x1A,
+	/** The requested Recovery Type is not defined in AVDTP */
+	BT_AVDTP_BAD_RECOVERY_TYPE = 0x22,
+	/** The format of Media Transport Capability is not correct */
+	BT_AVDTP_BAD_MEDIA_TRANSPORT_FORMAT = 0x23,
+	/** The format of Recovery Service Capability is not correct */
+	BT_AVDTP_BAD_RECOVERY_FORMAT = 0x25,
+	/** The format of Header Compression Service Capability is not correct */
+	BT_AVDTP_BAD_ROHC_FORMAT = 0x26,
+	/** The format of Content Protection Service Capability is not correct */
+	BT_AVDTP_BAD_CP_FORMAT = 0x27,
+	/** The format of Multiplexing Service Capability is not correct */
+	BT_AVDTP_BAD_MULTIPLEXING_FORMAT = 0x28,
+	/** Configuration not supported */
+	BT_AVDTP_UNSUPPORTED_CONFIGURAION = 0x29,
+	/** Indicates that the ACP state machine is in an invalid state in order to process the
+	 * signal. This also includes the situation when an INT receives a request for the
+	 * same command that it is currently expecting a response
+	 */
+	BT_AVDTP_BAD_STATE = 0x31,
 };
 
-/** @brief AVDTP Stream */
-struct bt_avdtp_stream {
-	struct bt_l2cap_br_chan chan; /* Transport Channel*/
-	struct bt_avdtp_seid_info lsep; /* Configured Local SEP */
-	struct bt_avdtp_seid_info rsep; /* Configured Remote SEP*/
-	uint8_t state; /* current state of the stream */
-	struct bt_avdtp_stream *next;
+/** @brief Stream End Point Type */
+enum bt_avdtp_sep_type {
+	/** Source Role */
+	BT_AVDTP_SOURCE = 0,
+	/** Sink Role */
+	BT_AVDTP_SINK = 1
+};
+
+/** @brief Stream End Point Media Type */
+enum bt_avdtp_media_type {
+	/** Audio Media Type */
+	BT_AVDTP_AUDIO = 0x00,
+	/** Video Media Type */
+	BT_AVDTP_VIDEO = 0x01,
+	/** Multimedia Media Type */
+	BT_AVDTP_MULTIMEDIA = 0x02
+};
+
+/** @brief AVDTP stream endpoint information.
+ *  Don't need to care endianness because it is not used for data parsing.
+ */
+struct bt_avdtp_sep_info {
+	/** End Point usage status */
+	uint8_t inuse:1;
+	/** Stream End Point ID that is the identifier of the stream endpoint */
+	uint8_t id:6;
+	/** Reserved */
+	uint8_t reserved:1;
+	/** Stream End-point Type that indicates if the stream end-point is SNK or SRC */
+	enum bt_avdtp_sep_type tsep;
+	/** Media-type of the End Point
+	 * Only @ref BT_AVDTP_AUDIO is supported now.
+	 */
+	enum bt_avdtp_media_type media_type;
+};
+
+/** @brief service category Type */
+enum bt_avdtp_service_category {
+	/** Media Transport */
+	BT_AVDTP_SERVICE_MEDIA_TRANSPORT = 0x01,
+	/** Reporting */
+	BT_AVDTP_SERVICE_REPORTING = 0x02,
+	/** Recovery */
+	BT_AVDTP_SERVICE_MEDIA_RECOVERY = 0x03,
+	/** Content Protection */
+	BT_AVDTP_SERVICE_CONTENT_PROTECTION = 0x04,
+	/** Header Compression */
+	BT_AVDTP_SERVICE_HEADER_COMPRESSION = 0x05,
+	/** Multiplexing */
+	BT_AVDTP_SERVICE_MULTIPLEXING = 0x06,
+	/** Media Codec */
+	BT_AVDTP_SERVICE_MEDIA_CODEC = 0x07,
+	/** Delay Reporting */
+	BT_AVDTP_SERVICE_DELAY_REPORTING = 0x08,
+};
+
+/** @brief AVDTP Stream End Point */
+struct bt_avdtp_sep {
+	/** Stream End Point information */
+	struct bt_avdtp_sep_info sep_info;
+	/** Media Transport Channel*/
+	struct bt_l2cap_br_chan chan;
+	/** the endpoint media data */
+	void (*media_data_cb)(struct bt_avdtp_sep *sep,
+				struct net_buf *buf);
+	/** avdtp session */
+	struct bt_avdtp *session;
+	/** SEP state */
+	uint8_t state;
+	/* Internally used list node */
+	sys_snode_t _node;
 };
 
 #ifdef __cplusplus

subsys/bluetooth/host/classic/avdtp_internal.h
@@ -2,6 +2,7 @@
  * avdtp_internal.h - avdtp handling
 
  * Copyright (c) 2015-2016 Intel Corporation
+ * Copyright 2021,2024 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -82,22 +83,38 @@
 #define BT_AVDTP_ERR_UNSUPPORTED_CONFIGURAION   0x29
 #define BT_AVDTP_ERR_BAD_STATE                  0x31
 
-#define BT_AVDTP_MAX_MTU BT_L2CAP_RX_MTU
-
 #define BT_AVDTP_MIN_SEID 0x01
 #define BT_AVDTP_MAX_SEID 0x3E
 
 struct bt_avdtp;
 struct bt_avdtp_req;
+struct bt_avdtp_sep_info;
 
-typedef int (*bt_avdtp_func_t)(struct bt_avdtp *session,
-			       struct bt_avdtp_req *req);
+/** @brief AVDTP SEID Information AVDTP_SPEC V13 Table 8.8 */
+struct bt_avdtp_sep_data {
+#ifdef CONFIG_LITTLE_ENDIAN
+	uint8_t rfa0:1;
+	uint8_t inuse:1;
+	uint8_t id:6;
+	uint8_t rfa1:3;
+	uint8_t tsep:1;
+	uint8_t media_type:4;
+#else
+	uint8_t id:6;
+	uint8_t inuse:1;
+	uint8_t rfa0:1;
+	uint8_t media_type:4;
+	uint8_t tsep:1;
+	uint8_t rfa1:3;
+#endif
+} __packed;
+
+typedef int (*bt_avdtp_func_t)(struct bt_avdtp_req *req);
 
 struct bt_avdtp_req {
 	uint8_t sig;
 	uint8_t tid;
 	bt_avdtp_func_t func;
-	struct k_work_delayable timeout_work;
 };
 
 struct bt_avdtp_single_sig_hdr {
@@ -105,47 +122,108 @@ struct bt_avdtp_single_sig_hdr {
 	uint8_t signal_id;
 } __packed;
 
-#define BT_AVDTP_SIG_HDR_LEN sizeof(struct bt_avdtp_single_sig_hdr)
-
-struct bt_avdtp_ind_cb {
-	/*
-	 * discovery_ind;
-	 * get_capabilities_ind;
-	 * set_configuration_ind;
-	 * open_ind;
-	 * start_ind;
-	 * suspend_ind;
-	 * close_ind;
-	 */
-};
-
-struct bt_avdtp_cap {
-	uint8_t cat;
-	uint8_t len;
-	uint8_t data[0];
-};
-
-struct bt_avdtp_sep {
-	uint8_t seid;
-	uint8_t len;
-	struct bt_avdtp_cap caps[0];
-};
+struct bt_avdtp_media_hdr {
+#ifdef CONFIG_LITTLE_ENDIAN
+	uint8_t CSRC_count:4;
+	uint8_t header_extension:1;
+	uint8_t padding:1;
+	uint8_t RTP_version:2;
+	uint8_t playload_type:7;
+	uint8_t marker:1;
+#else
+	uint8_t RTP_version:2;
+	uint8_t padding:1;
+	uint8_t header_extension:1;
+	uint8_t CSRC_count:4;
+	uint8_t marker:1;
+	uint8_t playload_type:7;
+#endif
+	uint16_t sequence_number;
+	uint32_t time_stamp;
+	uint32_t synchronization_source;
+} __packed;
 
 struct bt_avdtp_discover_params {
 	struct bt_avdtp_req req;
 	uint8_t status;
-	struct bt_avdtp_sep *caps;
+	struct net_buf *buf;
+};
+
+struct bt_avdtp_get_capabilities_params {
+	struct bt_avdtp_req req;
+	uint8_t status;
+	uint8_t stream_endpoint_id;
+	struct net_buf *buf;
+};
+
+struct bt_avdtp_set_configuration_params {
+	struct bt_avdtp_req req;
+	struct bt_avdtp_sep *sep;
+	uint8_t status;
+	uint8_t acp_stream_ep_id;
+	uint8_t int_stream_endpoint_id;
+	uint8_t media_type;
+	uint8_t media_codec_type;
+	uint8_t codec_specific_ie_len;
+	uint8_t *codec_specific_ie;
+};
+
+struct bt_avdtp_open_params {
+	struct bt_avdtp_req req;
+	struct bt_avdtp_sep *sep;
+	uint8_t status;
+	uint8_t acp_stream_ep_id;
+};
+
+struct bt_avdtp_start_params {
+	struct bt_avdtp_req req;
+	struct bt_avdtp_sep *sep;
+	uint8_t status;
+	uint8_t acp_stream_ep_id;
+};
+
+struct bt_avdtp_ops_cb {
+	void (*connected)(struct bt_avdtp *session);
+
+	void (*disconnected)(struct bt_avdtp *session);
+
+	struct net_buf *(*alloc_buf)(struct bt_avdtp *session);
+
+	int (*discovery_ind)(struct bt_avdtp *session, uint8_t *errcode);
+
+	int (*get_capabilities_ind)(struct bt_avdtp *session,
+		struct bt_avdtp_sep *sep, struct net_buf *rsp_buf, uint8_t *errcode);
+
+	int (*set_configuration_ind)(struct bt_avdtp *session, struct bt_avdtp_sep *sep,
+		uint8_t int_seid, struct net_buf *buf, uint8_t *errcode);
+
+	int (*open_ind)(struct bt_avdtp *session,
+		struct bt_avdtp_sep *sep, uint8_t *errcode);
+
+	int (*close_ind)(struct bt_avdtp *session,
+		struct bt_avdtp_sep *sep, uint8_t *errcode);
+
+	int (*start_ind)(struct bt_avdtp *session,
+		struct bt_avdtp_sep *sep, uint8_t *errcode);
+
+	int (*suspend_ind)(struct bt_avdtp *session,
+		struct bt_avdtp_sep *sep, uint8_t *errcode);
+
+	int (*abort_ind)(struct bt_avdtp *session,
+		struct bt_avdtp_sep *sep, uint8_t *errcode);
 };
 
 /** @brief Global AVDTP session structure. */
 struct bt_avdtp {
 	struct bt_l2cap_br_chan br_chan;
-	struct bt_avdtp_stream *streams; /* List of AV streams */
 	struct bt_avdtp_req *req;
+	const struct bt_avdtp_ops_cb *ops;
+	struct bt_avdtp_sep *current_sep;
+	struct k_work_delayable timeout_work;
+	uint8_t signalling_l2cap_connected;
 };
 
 struct bt_avdtp_event_cb {
-	struct bt_avdtp_ind_cb *ind;
 	int (*accept)(struct bt_conn *conn, struct bt_avdtp **session);
 };
 
@@ -163,8 +241,41 @@ int bt_avdtp_disconnect(struct bt_avdtp *session);
 
 /* AVDTP SEP register function */
 int bt_avdtp_register_sep(uint8_t media_type, uint8_t role,
-				struct bt_avdtp_seid_lsep *sep);
+				struct bt_avdtp_sep *sep);
 
 /* AVDTP Discover Request */
 int bt_avdtp_discover(struct bt_avdtp *session,
 		      struct bt_avdtp_discover_params *param);
+
+/* Parse the sep of discovered result */
+int bt_avdtp_parse_sep(struct net_buf *buf, struct bt_avdtp_sep_info *sep_info);
+
+/* AVDTP Get Capabilities */
+int bt_avdtp_get_capabilities(struct bt_avdtp *session,
+		      struct bt_avdtp_get_capabilities_params *param);
+
+/* Parse the codec type of capabilities */
+int bt_avdtp_parse_capability_codec(struct net_buf *buf, uint8_t *codec_type,
+				uint8_t **codec_info_element, uint16_t *codec_info_element_len);
+
+/* AVDTP Set Configuration */
+int bt_avdtp_set_configuration(struct bt_avdtp *session,
+		      struct bt_avdtp_set_configuration_params *param);
+
+/* AVDTP reconfigure */
+int bt_avdtp_reconfigure(struct bt_avdtp *session,
+		      struct bt_avdtp_set_configuration_params *param);
+
+/* AVDTP OPEN */
+int bt_avdtp_open(struct bt_avdtp *session,
+		      struct bt_avdtp_open_params *param);
+
+/* AVDTP START */
+int bt_avdtp_start(struct bt_avdtp *session,
+		      struct bt_avdtp_start_params *param);
+
+/* AVDTP send data */
+int bt_avdtp_send_media_data(struct bt_avdtp_sep *sep, struct net_buf *buf);
+
+/* get media l2cap connection MTU */
+uint32_t bt_avdtp_get_media_mtu(struct bt_avdtp_sep *sep);